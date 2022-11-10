6c9803ea638306f72ea183620d1569932d12dd99
jess.wu <jess.wu@quad.tw>
2022-11-10 09:39:14 +0800

    update api path

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('store', [LeaveController::class, 'store']);
+Route::prefix('leave')->group(function () {
+	Route::post('store', [LeaveController::class, 'store']);
+});
