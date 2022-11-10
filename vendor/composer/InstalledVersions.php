17ca472afc34b39851aea3396de73b01369be67a
jess.wu <jess.wu@quad.tw>
2022-11-10 10:31:43 +0800

    update vendor

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -778,8 +778,8 @@ private static $installed = array (
     array (
       'provided' => 
       array (
-        0 => '1.0|2.0|3.0',
-        1 => '1.0.0 || 2.0.0 || 3.0.0',
+        0 => '1.0.0 || 2.0.0 || 3.0.0',
+        1 => '1.0|2.0|3.0',
       ),
     ),
     'psr/simple-cache' => 
